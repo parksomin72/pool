727fc312c4a3ac1343af63e4e9dfbd9160b415db
parksomin72 <fatimazahraealwi@gmail.com>
2025-04-24 14:50:56 -0700

    .

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -9,14 +9,13 @@ void ft_putnbr(int nb)
 {
 	if (nb < 0)
 	{
-		ft_putchar('-');
 		if (nb == -2147483648)
-		{
-			ft_putchar('2');
-			nb = 147483648;
-		}
+			write(1, "-2147483648", 11);
 		else
+		{
+			ft_putchar('-');
 			nb *= -1;
+		}
 	}
 	if (nb >= 10)
 	{
